064de1023bf1fb3144899903c8c08ce1895ec374
Magnar Sveen <magnars@gmail.com>
2013-12-08 20:00:03 +0100

    Release 0.1.2

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject optimus-angular "0.1.1"
+(defproject optimus-angular "0.1.2"
   :description "Angular.JS optimizations for Optimus"
   :url "http://github.com/magnars/optimus-angular"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [optimus "0.12.3"]]
+                 [optimus "0.12.6"]]
   :profiles {:dev {:dependencies [[midje "1.5.0"]]
                    :plugins [[lein-midje "3.0.0"]
                              [lein-shell "0.3.0"]]}}
