faac8e883fecb9df0b24626cb159559d3e892606
Magnar Sveen <magnars@gmail.com>
2023-02-08 10:03:21 +0100

    Remove optimus as direct dependency - require your own instead

--- project.clj
+++ project.clj
@@ -3,9 +3,10 @@
   :url "http://github.com/magnars/optimus-angular"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[optimus "1.0.0-rc3"]]
+  :dependencies []
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]
-                                  [midje "1.9.9"]]
+                                  [midje "1.9.9"]
+                                  [optimus "2023-02-08"]]
                    :plugins [[lein-midje "3.2.1"]
                              [lein-shell "0.5.0"]]}}
   :prep-tasks [["shell" "./build-js-sources.sh"]])
